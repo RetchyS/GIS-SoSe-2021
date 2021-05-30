783a47e26d3262258d8ee34223d4aed5022b86a7
Retchy <sascha.martin.haeder@hs-furtwangen.de>
2021-05-30 19:25:48 +0200

    dddd

--- Aufgabe3.1/script31.js
+++ Aufgabe3.1/script31.js
@@ -5,22 +5,40 @@ const Http = require("http");
 var Aufgabe31;
 (function (Aufgabe31) {
     console.log("Starting server");
-    let port = Number(process.env.PORT);
-    if (!port)
+    let port = Number(process.env.PORT); // Holt sich den Port aus dem User Environment. Rückgabe ist ein String und wird mit nem Cast zur Zahl
+    if (!port) // Falls kein Port hinterlegt ist bzw. die Variable Port undefined ist                
         port = 8100;
-    let server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
+    let server = Http.createServer(); // Server wird erstellt um listener hinzufügen zu können
+    server.addListener("request", handleRequest); // Request/Anfragenhandler für den Server
+    server.addListener("listening", handleListen); // Listenerhandler für den Server
     server.listen(port);
     function handleListen() {
         console.log("Listening");
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
+        console.log("I hear voices!"); // Serverlog
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
         _response.end();
+    }
+    // Damit der Button des Formulars die FormData function ausführt
+    let button = document.getElementById("senden");
+    button.addEventListener("click", formData);
+    async function formData() {
+        let formular = new FormData(document.forms[0]); // Holt sich die Information des ersten Formulars, in dem Fall 0, und packt es in die Variable formular
+        for (let entry of formular) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+        let query = new URLSearchParams(FormData); //any weil Javascript Formdata net kennt
+        let url = "https://piikachu.herokuapp.com/";
+        url = url + "?" + query.toString();
+        let response = await fetch(url);
+        let responsetext = await response.text(); // Serverantwort in Text kovertieren
+        let antwort = document.getElementById("serverantwort");
+        antwort.innerText = responsetext;
     }
 })(Aufgabe31 = exports.Aufgabe31 || (exports.Aufgabe31 = {}));
 //# sourceMappingURL=script31.js.map
--- Aufgabe3.1/script31.ts
+++ Aufgabe3.1/script31.ts
@@ -2,25 +2,57 @@ import * as Http from "http";
 
 export namespace Aufgabe31 {
     console.log("Starting server");
-    let port: number = Number(process.env.PORT);
-    if (!port)
+    let port: number = Number(process.env.PORT);    // Holt sich den Port aus dem User Environment. Rückgabe ist ein String und wird mit nem Cast zur Zahl
+    if (!port)                                      // Falls kein Port hinterlegt ist bzw. die Variable Port undefined ist                
         port = 8100;
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
+    let server: Http.Server = Http.createServer();      // Server wird erstellt um listener hinzufügen zu können
+    server.addListener("request", handleRequest);      // Request/Anfragenhandler für den Server
+    server.addListener("listening", handleListen);     // Listenerhandler für den Server
     server.listen(port);
 
-    function handleListen(): void {
+    function handleListen(): void {                     // Die Funktion von listener
         console.log("Listening");
     }
 
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {      // Die funktion von Request
+        console.log("I hear voices!");                  // Serverlog
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
         _response.end();
     }
+
+    // Damit der Button des Formulars die FormData function ausführt
+    let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("senden");
+    button.addEventListener("click", formData);
+
+    async function formData(): Promise<void> {                        // Neue Funktion da await nur mit asyn funktioniert
+        let formular: FormData = new FormData(document.forms[0]); // Holt sich die Information des ersten Formulars, in dem Fall 0, und packt es in die Variable formular
+
+        for (let entry of formular) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+
+        let query: URLSearchParams = new URLSearchParams(<any> FormData);    //any weil Javascript Formdata net kennt
+        let url: RequestInfo = "https://piikachu.herokuapp.com/";
+        url = url + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let responsetext: string = await response.text();       // Serverantwort in Text kovertieren
+        let antwort: HTMLElement = document.getElementById("serverantwort");
+        antwort.innerText = responsetext;
+    }
+
+
+
+
+
+
+
+
+
+
 }
