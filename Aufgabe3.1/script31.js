bcb63601be3cfdf011eb65b9e127ad76d9e52f66
Retchy <sascha.martin.haeder@hs-furtwangen.de>
2021-05-30 18:17:43 +0200

    ddd

--- Aufgabe3.1/script31.js
+++ Aufgabe3.1/script31.js
@@ -4,6 +4,23 @@ exports.Aufgabe31 = void 0;
 const Http = require("http");
 var Aufgabe31;
 (function (Aufgabe31) {
-    Http.createServer(Aufgabe31);
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
+        _response.end();
+    }
 })(Aufgabe31 = exports.Aufgabe31 || (exports.Aufgabe31 = {}));
 //# sourceMappingURL=script31.js.map
