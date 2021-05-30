a03df66ac2c7ecce73ef892c8083b607e25e2e66
Retchy <sascha.martin.haeder@hs-furtwangen.de>
2021-05-30 17:59:29 +0200

    test

---
+++ Aufgabe3.1/script31.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Aufgabe31 = void 0;
+const Http = require("http");
+var Aufgabe31;
+(function (Aufgabe31) {
+    Http.createServer(Aufgabe31);
+})(Aufgabe31 = exports.Aufgabe31 || (exports.Aufgabe31 = {}));
+//# sourceMappingURL=script31.js.map
---
+++ Aufgabe3.1/script31.ts
@@ -0,0 +1,10 @@
+import * as Http from "http";
+
+
+export namespace Aufgabe31 {
+Http.createServer(Aufgabe31);
+
+
+
+    
+}
