07c58569a126225b00e7a115b88973178c4b7ba4
Retchy <sascha.martin.haeder@hs-furtwangen.de>
2021-05-30 20:04:03 +0200

    ddd

--- Aufgabe3.1/script31.js
+++ Aufgabe3.1/script31.js
@@ -1,9 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Aufgabe31 = void 0;
+exports.Aufgabe31Server = void 0;
 const Http = require("http");
-var Aufgabe31;
-(function (Aufgabe31) {
+// Damit der Button des Formulars die FormData function ausführt
+let button = document.getElementById("senden");
+button.addEventListener("click", formData);
+async function formData() {
+    let formular = new FormData(document.forms[0]); // Holt sich die Information des ersten Formulars, in dem Fall 0, und packt es in die Variable formular
+    for (let entry of formular) {
+        console.log(entry);
+        console.log("name: " + entry[0]);
+        console.log("value: " + entry[1]);
+    }
+    let query = new URLSearchParams(FormData); //any weil Javascript Formdata net kennt
+    let url = "https://piikachu.herokuapp.com/";
+    url = url + "?" + query.toString();
+    let response = await fetch(url);
+    let responsetext = await response.text(); // Serverantwort in Text kovertieren
+    let antwort = document.getElementById("serverantwort");
+    antwort.innerText = responsetext;
+}
+var Aufgabe31Server;
+(function (Aufgabe31Server) {
     console.log("Starting server");
     let port = Number(process.env.PORT); // Holt sich den Port aus dem User Environment. Rückgabe ist ein String und wird mit nem Cast zur Zahl
     if (!port) // Falls kein Port hinterlegt ist bzw. die Variable Port undefined ist                
@@ -22,5 +40,5 @@ var Aufgabe31;
         _response.write(_request.url);
         _response.end();
     }
-})(Aufgabe31 = exports.Aufgabe31 || (exports.Aufgabe31 = {}));
+})(Aufgabe31Server = exports.Aufgabe31Server || (exports.Aufgabe31Server = {}));
 //# sourceMappingURL=script31.js.map
