b7fadc2d440e87ccd66d54adeae6e4009d7886de
Retchy <sascha.martin.haeder@hs-furtwangen.de>
2021-07-07 22:56:37 +0200

    ddddd

--- Memory/spiel.js
+++ Memory/spiel.js
@@ -60,27 +60,59 @@ var Memory;
         }
     }
     //------------Spiel-------------------------------------
+    let buttonstart = document.getElementById("start");
+    buttonstart.addEventListener("click", spielfeld);
     //Spielfeld generieren
-    let aktuellekarte;
-    let cardid = "cardid";
-    function spielfeld() {
+    async function spielfeld() {
+        let formular = new FormData(document.forms[0]);
+        let bilderposi = "card";
+        let query = new URLSearchParams(formular);
+        let url = "https://piikachu.herokuapp.com";
+        url += "/abfragen";
+        url = url + "?" + query.toString();
+        let response = await fetch(url);
+        let responsetext = await response.json();
+        console.log(responsetext.length);
+        console.log(responsetext);
+        //Auswahl der Karten
+        let duplicatezwei = null;
+        let spielkartensrc = null; //Die 8 Karten aus dem Datensatz werden ausgewählt und hier gespeichert
+        let duplicatenumber = 0; //Keiner der 8 dürfen gleich sein
+        let randomindex = 0;
+        for (let x = 0; x < 8; x++) {
+            randomindex = Math.floor((Math.random() * responsetext.length) + 0);
+            let srcstring = responsetext[randomindex].Bilderlink;
+            spielkartensrc.forEach(srcstring => {
+                duplicatenumber++;
+            });
+            if (duplicatenumber < 2) {
+                spielkartensrc.push(srcstring);
+                duplicatenumber = 0;
+            }
+        }
+        //Karten hinzufügen
+        let counterzwei = 0;
         for (let i = 0; i < 16; i++) { // 16 für die Kartenanzahl
-            cardid += i;
-            kartenid.value = cardid;
-            kartenbild.setAttributeNode(kartenid);
-            aktuellekarte = document.getElementById(cardid);
-            kartedatenbank(); //SRC für die karte wird gesucht
-            kartenbild.setAttributeNode(kartensrc);
-            aktuellekarte.appendChild(kartenbild);
+            bilderposi += i;
+            randomindex = Math.floor((Math.random() * 7) + 0); //7 da 0 dazu zählt, für 8 verschiedene karten
+            let src = spielkartensrc[randomindex];
+            duplicatezwei.push(src);
+            duplicatezwei.forEach(src => {
+                counterzwei++;
+            });
+            if (counterzwei < 3) {
+                kartehinzufügen(src, bilderposi);
+                counterzwei = 0;
+            }
         }
     }
-    //Serverdaten abfrage
-    async function kartedatenbank() {
-        let url = "https://piikachu.herokuapp.com";
-        url += "/suchen";
-        let response = await fetch(url);
-        let responsetext = await response.text();
-        console.log(responsetext);
+    function kartehinzufügen(_src, _bilderposi) {
+        let img = document.createElement("img");
+        let imgcontainer = document.getElementById(_bilderposi);
+        let imgsrc = document.createAttribute("src");
+        imgsrc.value = _src;
+        img.setAttributeNode(imgsrc);
+        imgcontainer.appendChild(img);
     }
 })(Memory || (Memory = {}));
 //schaut nach einem match
--- Memory/spiel.ts
+++ Memory/spiel.ts
@@ -62,7 +62,7 @@ namespace Memory {
             let kartendatenbank = document.createElement("img");
             let kartendiv = document.createElement("div");
             let kartenclass = document.createAttribute("class");
-            
+
             let kartendatenbanksrc = document.createAttribute("src");
             let kartendatenbankid = document.createAttribute("id");
 
@@ -78,7 +78,7 @@ namespace Memory {
 
             bilderantwort.appendChild(kartendiv);
             kartendiv.appendChild(kartendatenbank);
-            
+
         }
     }
 
@@ -90,37 +90,76 @@ namespace Memory {
 
 
     //------------Spiel-------------------------------------
-
+    let buttonstart: HTMLButtonElement = <HTMLButtonElement>document.getElementById("start");
+    buttonstart.addEventListener("click", spielfeld);
     //Spielfeld generieren
-    let aktuellekarte;
-    let cardid: string = "cardid";
+    async function spielfeld(): Promise<void> {
 
-    function spielfeld(): void {
-        for (let i: number = 0; i < 16; i++) { // 16 für die Kartenanzahl
+        let formular: FormData = new FormData(document.forms[0]);
+        let bilderposi: string = "card";
 
-            cardid += i;
-            kartenid.value = cardid;
-            kartenbild.setAttributeNode(kartenid);
-
-            aktuellekarte = document.getElementById(cardid);
-
-            kartedatenbank();           //SRC für die karte wird gesucht
-
-            kartenbild.setAttributeNode(kartensrc);
-
-            aktuellekarte.appendChild(kartenbild);
-        }
-    }
-
-    //Serverdaten abfrage
-    async function kartedatenbank(): Promise<void> {
+        let query: URLSearchParams = new URLSearchParams(<any>formular);
         let url: RequestInfo = "https://piikachu.herokuapp.com";
-        url += "/suchen";
+        url += "/abfragen";
+        url = url + "?" + query.toString();
         let response: Response = await fetch(url);
-        let responsetext: string = await response.text();
+        let responsetext: Data[] = await response.json();
+        console.log(responsetext.length);
         console.log(responsetext);
 
+        //Auswahl der Karten
+        let duplicatezwei: string[] = null;
+        let spielkartensrc: string[] = null;         //Die 8 Karten aus dem Datensatz werden ausgewählt und hier gespeichert
+        let duplicatenumber: number = 0;            //Keiner der 8 dürfen gleich sein
+        let randomindex: number = 0;
+        for (let x: number = 0; x < 8; x++) {
+            randomindex = Math.floor((Math.random() * responsetext.length) + 0);
+            let srcstring: string = responsetext[randomindex].Bilderlink;
+
+            spielkartensrc.forEach(srcstring => {
+                duplicatenumber++;
+            });
+
+            if (duplicatenumber < 2) {
+
+                spielkartensrc.push(srcstring);
+
+                duplicatenumber = 0;
+
+            }
+        }
+
+        //Karten hinzufügen
+        let counterzwei: number = 0;
+        for (let i: number = 0; i < 16; i++) { // 16 für die Kartenanzahl
+
+            bilderposi += i;
+            randomindex = Math.floor((Math.random() * 7) + 0); //7 da 0 dazu zählt, für 8 verschiedene karten
+            let src: string = spielkartensrc[randomindex];
+            duplicatezwei.push(src);
+            duplicatezwei.forEach(src => {
+                counterzwei++;
+            });
+            if (counterzwei < 3) {
+                kartehinzufügen(src, bilderposi);
+                counterzwei = 0;
+            }
+        }
     }
+    function kartehinzufügen(_src: string, _bilderposi: string): void {
+        let img = document.createElement("img");
+        let imgcontainer = document.getElementById(_bilderposi);
+        let imgsrc = document.createAttribute("src");
+
+        imgsrc.value = _src;
+        img.setAttributeNode(imgsrc);
+
+        imgcontainer.appendChild(img);
+
+
+
+    }
+
 }
 
 
