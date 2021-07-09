c5cf0cdda4490267f6a67233c0efec95ca8d4104
Retchy <sascha.martin.haeder@hs-furtwangen.de>
2021-07-09 21:48:56 +0200

    dwasdwas

--- Memory/spiel.js
+++ Memory/spiel.js
@@ -7,31 +7,11 @@ var MemorySpiel;
     let buttonstart = document.getElementById("start");
     buttonstart.addEventListener("click", spielfeld);
     //Spielfeld generieren
+    let randomzahl;
+    let randomzahlen;
+    let doppelwerte;
+    let spielkartenarrayzahlen;
     //Spielkarten
-    function kartenhinzufügen(_response) {
-        let spielkarten;
-        let spielkarte;
-        let randomindex;
-        let randomindexe;
-        let doppel = false;
-        for (let i = 0; i < 8; i++) {
-            spielkarte = _response[i].Bilderlink;
-            spielkarten[i] = spielkarte;
-        }
-        console.log(spielkarten);
-        let counter;
-        for (let i = 0; i < 16; i++) {
-            let kartendatenbank = document.createElement("img");
-            let kartenclass = document.createAttribute("class");
-            let kartendatenbanksrc = document.createAttribute("src");
-            kartendatenbanksrc.value = spielkarten[i];
-            kartenclass.value = "karte";
-            console.log(kartendatenbanksrc);
-            let kartendiv = document.getElementById("cardid" + i);
-            kartendatenbank.setAttributeNode(kartendatenbanksrc);
-            kartendiv.appendChild(kartendatenbank);
-        }
-    }
     async function spielfeld() {
         let formular = new FormData(document.forms[0]);
         let bilderposi = "card";
@@ -49,93 +29,36 @@ var MemorySpiel;
         }).then(srcarray => {
             console.log(srcarray.length);
             console.log(srcarray);
-            kartenhinzufügen(srcarray);
+            spielkartenarrayzahlen = randomindexarray(srcarray.length);
+            kartenhinzufügen(srcarray, spielkartenarrayzahlen);
         });
-        /*  for (let i: number = 0; i < 16; i++) {
- 
- 
-             let kartendatenbank = document.createElement("img");
-             let kartenclass = document.createAttribute("class");
-             let kartendatenbanksrc = document.createAttribute("src");
- 
-             let kartendatenbankid = document.createAttribute("id");
- 
-             console.log(responsetext[i].Bilderlink);
- 
-             kartendatenbanksrc.value = responsetext[i].Bilderlink;
-             kartendatenbankid.value = "card" + i;
-             kartenclass.value = "karte";
-             console.log(kartendatenbanksrc);
-             let kartendiv = document.getElementById(kartendatenbankid.value);
- 
-             kartendatenbank.setAttributeNode(kartendatenbanksrc);
- 
-             kartendiv.appendChild(kartendatenbank);
- 
-         } */
-        //Auswahl der Karten
-        /*  let spielkartensrc: Data[] =  responsetext;         //Die 8 Karten aus dem Datensatz werden ausgewählt und hier gespeichert
-         spielkartensrc.length = 7;
-         let benutzterindex: number[] = null;
-         benutzterindex.length = 7;
-         let randomindex: number = 0;
-         let doppel: boolean = false;
-         if (response != undefined) {
-             for (let x: number = 0; x < 8; x++) {
-                 randomindex = Math.floor((Math.random() * responsetext.length) + 0);
-                 spielkartensrc[x].Bilderlink = responsetext[randomindex].Bilderlink;
-    
-             }
-             console.log(benutzterindex);
-    
-             console.log(spielkartensrc);
-    
-    
-    
-    
-         } */
-        /* //Karten hinzufügen
-        let benutztekarten: number[];
-        let counterzwei: number = 0;
-        for (let i: number = 0; i < 16; i++) { // 16 für die Kartenanzahl
-            bilderposi += i;
-            randomindex = Math.floor((Math.random() * 7) + 0); //7 da 0 dazu zählt, für 8 verschiedene karten
-            benutztekarten.push(randomindex);
-            benutztekarten.forEach(countindex => {
-                countindex = randomindex;
-                counterzwei++;
-    
-            });
-    
-            let src: string = spielkartensrc[randomindex];
-    
-            if (counterzwei < 3) {
-                kartehinzufügen(src, bilderposi);
-                counterzwei = 0;
-            } else {
-                i--;
-            }
-        } */
     }
-    /*    function kartehinzufügen(_src: string, _bilderposi: string): void {
-           let img = document.createElement("img");
-           let imgcontainer = document.getElementById(_bilderposi);
-           let imgsrc = document.createAttribute("src");
-   
-           imgsrc.value = _src;
-           img.setAttributeNode(imgsrc);
-   
-           imgcontainer.appendChild(img);
-   
-   
-   
-       }
-       function kartenaussuche(_karten: Data[]) {
-   
-       }
-    */
+    function kartenhinzufügen(_response, _spielkartenzahlen) {
+        for (let i = 0; i < 16; i++) {
+            let kartendatenbank = document.createElement("img");
+            let kartenclass = document.createAttribute("class");
+            let kartendatenbanksrc = document.createAttribute("src");
+            kartendatenbanksrc.value = _response[_spielkartenzahlen[i]].Bilderlink;
+            kartenclass.value = "karte";
+            console.log(kartendatenbanksrc);
+            let kartendiv = document.getElementById("cardid" + i);
+            kartendatenbank.setAttributeNode(kartendatenbanksrc);
+            kartendiv.appendChild(kartendatenbank);
+        }
+    }
+    function randomindexarray(_srcarray) {
+        randomzahl = Math.floor((Math.random() * _srcarray) + 0);
+        for (let x = 0; x < 8; x++) {
+            doppelwerte = randomzahlen.includes(randomzahl);
+            if (!doppelwerte) {
+                randomzahlen.push(randomzahl);
+                randomzahlen.push(randomzahl);
+            }
+            else {
+                x--;
+            }
+        }
+        return randomzahlen;
+    }
 })(MemorySpiel || (MemorySpiel = {}));
-//schaut nach einem match
-function match() {
-}
 //# sourceMappingURL=spiel.js.map
--- Memory/spiel.ts
+++ Memory/spiel.ts
@@ -19,50 +19,12 @@ namespace MemorySpiel {
     buttonstart.addEventListener("click", spielfeld);
 
     //Spielfeld generieren
+    let randomzahl: number;
+    let randomzahlen: number[];
+    let doppelwerte: boolean;
 
+    let spielkartenarrayzahlen: number [];
     //Spielkarten
-
-
-    function kartenhinzufügen(_response: Data[]): void {
-        let spielkarten: string[];
-       
-        let spielkarte: string;
-        let randomindex: number;
-        let randomindexe: number[];
-        
-        let doppel: boolean = false;
-
-        for (let i: number = 0; i < 8; i++) {
-            spielkarte = _response[i].Bilderlink;
-            spielkarten[i] = spielkarte;
-            
-        }
-        console.log(spielkarten);
-        let counter: number;
-
-        for (let i: number = 0; i < 16; i++) {
-            
-                let kartendatenbank = document.createElement("img");
-                let kartenclass = document.createAttribute("class");
-                let kartendatenbanksrc = document.createAttribute("src");
-
-
-                
-
-                kartendatenbanksrc.value = spielkarten[i];
-
-                kartenclass.value = "karte";
-                console.log(kartendatenbanksrc);
-                let kartendiv = document.getElementById("cardid" + i);
-
-                kartendatenbank.setAttributeNode(kartendatenbanksrc);
-
-                kartendiv.appendChild(kartendatenbank);
-            
-
-        }
-
-    }
     async function spielfeld(): Promise<void> {
 
         let formular: FormData = new FormData(document.forms[0]);
@@ -84,97 +46,51 @@ namespace MemorySpiel {
         }).then(srcarray => {
             console.log(srcarray.length);
             console.log(srcarray);
-            kartenhinzufügen(srcarray);
+            spielkartenarrayzahlen = randomindexarray(srcarray.length);
+            kartenhinzufügen(srcarray, spielkartenarrayzahlen);
         });
+    }
 
-        /*  for (let i: number = 0; i < 16; i++) {
- 
- 
-             let kartendatenbank = document.createElement("img");
-             let kartenclass = document.createAttribute("class");
-             let kartendatenbanksrc = document.createAttribute("src");
- 
-             let kartendatenbankid = document.createAttribute("id");
- 
-             console.log(responsetext[i].Bilderlink);
- 
-             kartendatenbanksrc.value = responsetext[i].Bilderlink;
-             kartendatenbankid.value = "card" + i;
-             kartenclass.value = "karte";
-             console.log(kartendatenbanksrc);
-             let kartendiv = document.getElementById(kartendatenbankid.value);
- 
-             kartendatenbank.setAttributeNode(kartendatenbanksrc);
- 
-             kartendiv.appendChild(kartendatenbank);
- 
-         } */
+    function kartenhinzufügen(_response: Data[], _spielkartenzahlen: number []): void {
+
+        for (let i: number = 0; i < 16; i++) {
+
+            let kartendatenbank = document.createElement("img");
+            let kartenclass = document.createAttribute("class");
+            let kartendatenbanksrc = document.createAttribute("src");
 
 
 
-        //Auswahl der Karten
 
-        /*  let spielkartensrc: Data[] =  responsetext;         //Die 8 Karten aus dem Datensatz werden ausgewählt und hier gespeichert
-         spielkartensrc.length = 7;
-         let benutzterindex: number[] = null;
-         benutzterindex.length = 7;
-         let randomindex: number = 0;
-         let doppel: boolean = false;
-         if (response != undefined) {
-             for (let x: number = 0; x < 8; x++) {
-                 randomindex = Math.floor((Math.random() * responsetext.length) + 0);
-                 spielkartensrc[x].Bilderlink = responsetext[randomindex].Bilderlink;
-    
-             }
-             console.log(benutzterindex);
-    
-             console.log(spielkartensrc);
-    
-    
-    
-    
-         } */
-        /* //Karten hinzufügen
-        let benutztekarten: number[];
-        let counterzwei: number = 0;
-        for (let i: number = 0; i < 16; i++) { // 16 für die Kartenanzahl
-            bilderposi += i;
-            randomindex = Math.floor((Math.random() * 7) + 0); //7 da 0 dazu zählt, für 8 verschiedene karten
-            benutztekarten.push(randomindex);
-            benutztekarten.forEach(countindex => {
-                countindex = randomindex;
-                counterzwei++;
-    
-            });
-    
-            let src: string = spielkartensrc[randomindex];
-    
-            if (counterzwei < 3) {
-                kartehinzufügen(src, bilderposi);
-                counterzwei = 0;
-            } else {
-                i--;
-            }
-        } */
+            kartendatenbanksrc.value = _response[_spielkartenzahlen[i]].Bilderlink;
+
+            kartenclass.value = "karte";
+            console.log(kartendatenbanksrc);
+            let kartendiv = document.getElementById("cardid" + i);
+
+            kartendatenbank.setAttributeNode(kartendatenbanksrc);
+
+            kartendiv.appendChild(kartendatenbank);
+
+
+        }
 
     }
-    /*    function kartehinzufügen(_src: string, _bilderposi: string): void {
-           let img = document.createElement("img");
-           let imgcontainer = document.getElementById(_bilderposi);
-           let imgsrc = document.createAttribute("src");
-   
-           imgsrc.value = _src;
-           img.setAttributeNode(imgsrc);
-   
-           imgcontainer.appendChild(img);
-   
-   
-   
-       }
-       function kartenaussuche(_karten: Data[]) {
-   
-       }
-    */
+    function randomindexarray(_srcarray: number): number [] {
+        randomzahl = Math.floor((Math.random() * _srcarray) + 0);
+
+
+        for (let x: number = 0; x < 8; x++) {
+            doppelwerte = randomzahlen.includes(randomzahl);
+            if (!doppelwerte) {
+                randomzahlen.push(randomzahl);
+                randomzahlen.push(randomzahl);
+            } else {
+                x--;
+            }
+        }
+        return randomzahlen;
+    }
 }
 
 
@@ -198,8 +114,3 @@ namespace MemorySpiel {
 
 
 
-
-//schaut nach einem match
-function match(): void {
-
-}
