80d6142e42518dbf40b16a86e03370f4c3a1bdbb
Retchy <sascha.martin.haeder@hs-furtwangen.de>
2021-07-11 16:42:25 +0200

    dwads

--- Memory/server/server.js
+++ Memory/server/server.js
@@ -54,7 +54,7 @@ var Memory;
             }
             if (path == "/highscoreabfragen") {
                 console.log("Datenbank wird abgefragt");
-                let answerdata = highscore.find().sort({ Zeit: 1 });
+                let answerdata = highscore.find().sort({ Zeit: +1 });
                 let answerarray = await answerdata.toArray();
                 _response.write(JSON.stringify(answerarray));
             }
